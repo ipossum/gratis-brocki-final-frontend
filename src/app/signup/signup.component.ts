17ec890494f344dc2522758c9a1fdd1fe5aaa6de
Alexander Huber <alexander.huber@bedag.ch>
2022-07-01 09:10:44 +0200

    Signup

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,5 +1,4 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
-import {Signup} from '../openapi-gen/model/signup';
 import {UserControllerService} from "../openapi-gen/api/userController.service";
 import {UserCreationDto} from "../openapi-gen/model/userCreationDto";
 
@@ -13,22 +12,16 @@ export class SignupComponent implements OnInit {
   constructor(private userService: UserControllerService) { }
   ngOnInit(): void {
   }
-  //model: Signup = new Signup();
   model: UserCreationDto = {};
   @ViewChild('f') form: any;
 
   onSubmit() {
-    if (this.form.valid) {
-      this.model.username = this.form.username;
-      this.model.email = this.form.email;
-      this.model.password = this.form.password;
-      this.model.phoneNumber = this.form.phoneNumber;
-
+    //if (this.form.valid) {
       this.userService.registerNewUser(this.model).subscribe(response => {
         console.log("Form Submitted!");
       })
       this.form.reset();
-    }
+   // }
   }
 }
 
