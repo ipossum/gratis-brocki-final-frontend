3a94811d0bf1999b5cb1610d388133b775d8f3c5
Andreas Blaser <andreas.blaser@outlook.com>
2022-07-07 22:22:28 +0200

    fix and clean up item.component.html and item.component.ts

--- src/app/item-creation/item-creation.component.ts
+++ src/app/item-creation/item-creation.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {ItemControllerService, ItemCreationDto} from "../openapi-gen";
 import {Router} from "@angular/router";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-item-creation',
@@ -9,14 +10,38 @@ import {Router} from "@angular/router";
 })
 export class ItemCreationComponent implements OnInit {
 
-  constructor(private itemService: ItemControllerService, private router: Router) {}
+  selectedFile: File;
+
+  constructor(private itemService: ItemControllerService,
+              private router: Router,
+              private httpClient: HttpClient) {}
 
   ngOnInit(): void {}
 
   model: ItemCreationDto = {};
   @ViewChild('f') form: any;
 
+  onFileChanged(event) {
+    this.selectedFile = event.target.files[0];
+  }
+
   onSubmit() {
+    // Upload image first --> TODO: combine these two posts
+    // FormData API provides methods to easily prepare form data to be sent with POST HTTP requests.
+    const uploadImageData = new FormData();
+    uploadImageData.append('file', this.selectedFile, this.selectedFile.name);
+    this.httpClient
+      .post('http://localhost:8080/api/v1/files', uploadImageData, {observe: 'response'})
+      .subscribe((response) => {
+          if (response.status === 200) {
+            console.log('Image upload successful');
+          } else {
+            console.log('Image upload failed');
+          }
+        }
+      );
+
+    // Upload item second
     if (this.form.valid) {
       this.itemService.createNewItem(this.model).subscribe(response => {
         console.log("Form Submitted!");
